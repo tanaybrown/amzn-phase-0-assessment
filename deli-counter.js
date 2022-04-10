ee73db8b6de72ed4941723776bc6ae793e3be80a
Tanya <tanaybrown@gmail.com>
2022-04-09 18:56:17 -0700

    Completed assignment

--- deli-counter.js
+++ deli-counter.js
@@ -1,17 +1,56 @@
 // 1. Write your functions here
+//Input = name of deli
+//output = making change to array
+//side effects = adding name to array
+
+const katzDeli = []
+
+function takeANumber(katzLine, customerName){
+    katzLine = katzLine.push(customerName)
+    for (let [index, customer] of katzDeli.entries()) {
+        if (customer === customer.name)
+            console.log('Welcome ${customer}.  You are number $(index += 1) in line.')
+    }  
+  
+}
+
+let lineQueue = []
+
+function line(katzLine) {
+    for (let i = 0; i > katzLine.length; i++) {
+        let customerEntry = katzLine[i];
+        lineQueue.push('' + [i + 1] + '.' + customerEntry)
+    }
+if (katzLine.length === 0) {
+    return "The line is currently empty"
+    }
+else lineupdate = line.slice(0, -2)
+    return "The line is currently:" + $(line)
+}
+
+
+function nowServing(katzLine) {
+    if (katzLine.length > 0) {
+      return"Currently serving " + katzLine.shift() + ".";
+    } else {
+        return "There is nobody waiting to be served!";
+    }
+  }
+
+
 
 
 // 2. Example Usage
 
-// const katzDeli = []
+//const katzDeli = []
 
-// takeANumber(katzDeli, "Ada") //=> Welcome, Ada. You are number 1 in line.
-// takeANumber(katzDeli, "Grace") //=> Welcome, Grace. You are number 2 in line.
-// takeANumber(katzDeli, "Kent") //=> Welcome, Kent. You are number 3 in line.
+//takeANumber(katzDeli, "Ada")
+//takeANumber(katzDeli, "Grace") 
+//takeANumber(katzDeli, "Kent"), Welcome, Kent. You are number 3 in line.
 
-// line(katzDeli) //=> "The line is currently: 1. Ada 2. Grace 3. Kent"
+//line(katzDeli), "The line is currently: 1. Ada 2. Grace 3. Kent"
 
-// nowServing(katzDeli) //=> "Currently serving Ada."
+//nowServing(katzDeli), "Currently serving Ada."
 
 // line(katzDeli) //=> "The line is currently: 1. Grace 2. Kent"
 
